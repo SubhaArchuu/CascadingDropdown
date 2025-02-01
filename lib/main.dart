2332d07d9b8a04bd4da04475d562481a13c83c9e
SUBA <subharchu236@gmail.com>
2025-02-01 13:09:08 +0530

    space added

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'dart:async';
 
-
 void main() {
   runApp(const MyApp());
 }
@@ -32,7 +31,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   String? dropdownvalue;
-  String? dropdownvalue2 ="";
+  String? dropdownvalue2 = "";
   String? dropdownId = "";
   List<OptionModel> dropdownData1 = [];
   List<OptionModel> dropdownData2 = [];
@@ -44,14 +43,14 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   Future<List<OptionModel>> getDropdown1Value() async {
-    var baseUrl = "/*use your 1st dropdown api url*/"; // eg., https://gssskhokhar.com/api/classes/
+    var baseUrl =
+        "/*use your 1st dropdown api url*/"; // eg., https://gssskhokhar.com/api/classes/
     var headers = {
       'Content-type': 'application/json',
     };
     var body = <String, String>{
       // Enter your body list here
       //eg., "data":"str",
-
     };
     var response = await http.post(
       Uri.parse(baseUrl),
@@ -61,12 +60,10 @@ class _MyHomePageState extends State<MyHomePage> {
     var jsonData = json.decode(response.body);
     print("jsonData -> ${jsonData}");
     dropdownData1 = [];
-    if(jsonData["message"] == ""){
+    if (jsonData["message"] == "") {
       for (var data in jsonData["options"]) {
-
-        OptionModel dropDownModelData = OptionModel(
-            id:data['id'],
-            val: data['value']);
+        OptionModel dropDownModelData =
+            OptionModel(id: data['id'], val: data['value']);
         dropdownData1.add(dropDownModelData);
       }
     }
@@ -75,26 +72,21 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   Future<List<OptionModel>> getDropdown2Value(String newvalue) async {
-
-    var appt =dropdownData1;
+    var appt = dropdownData1;
     var baseUrl = "/*use your 2nd dropdown api url*/";
     var headers = {
       'Content-type': 'application/json',
     };
-    if(newvalue != "") {
+    if (newvalue != "") {
       for (int i = 0; i < appt.length; i++) {
         if (appt[i].val == newvalue) {
           dropdownId = appt[i].id;
         }
       }
-    }
-    else
-    {
+    } else {
       dropdownId = "1";
     }
-    var body = <String, String>{
-      "data":dropdownId!
-    };
+    var body = <String, String>{"data": dropdownId!};
 
     var response = await http.post(
       Uri.parse(baseUrl),
@@ -104,13 +96,11 @@ class _MyHomePageState extends State<MyHomePage> {
     var jsonData = json.decode(response.body);
     print("jsonData -> ${jsonData}");
     dropdownData2 = [];
-    if(jsonData["message"] == ""){
+    if (jsonData["message"] == "") {
       for (var data in jsonData["options"]) {
-        OptionModel dropDownModelData = OptionModel(
-            id:data['id'],
-            val: data['value']);
+        OptionModel dropDownModelData =
+            OptionModel(id: data['id'], val: data['value']);
         dropdownData2.add(dropDownModelData);
-
       }
     }
 
@@ -127,91 +117,88 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-
             FutureBuilder<List<OptionModel>?>(
               future: getDropdown1Value(),
               builder: (context, snapshot) {
                 if (snapshot.hasData) {
                   var data = snapshot.data!;
-                  return
-                    Container(
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          border: Border.all(),
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        child: Padding(
-                            padding: const EdgeInsets.all(5.0),
-                            child: DropdownButtonHideUnderline(
-                                child:
-                                DropdownButton(
-                                  itemHeight: 50.0,
-                                  // Initial Value
-                                  value: dropdownvalue ?? data[0].val,
-                                  // Down Arrow Icon
-                                  icon: const Icon(Icons.keyboard_arrow_down),
+                  return Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        border: Border.all(),
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Padding(
+                          padding: const EdgeInsets.all(5.0),
+                          child: DropdownButtonHideUnderline(
+                              child: DropdownButton(
+                            itemHeight: 50.0,
+                            // Initial Value
+                            value: dropdownvalue ?? data[0].val,
+                            // Down Arrow Icon
+                            icon: const Icon(Icons.keyboard_arrow_down),
 
-                                  // Array list of items
-                                  items: data.map<DropdownMenuItem<String>>((OptionModel items) {
-                                    return DropdownMenuItem(
-                                      value: items.val,
-                                      child: Text(items.val!),
-                                    );
-                                  }).toList(),
-                                  // After selecting the desired option,it will
-                                  // change button value to selected value
-                                  onChanged: (String? newValue) {
-                                    setState(() {
-                                      dropdownvalue = newValue!;
-                                      getDropdown2Value(dropdownvalue!);
-                                    });
-                                  },
-                                ))));
+                            // Array list of items
+                            items: data.map<DropdownMenuItem<String>>(
+                                (OptionModel items) {
+                              return DropdownMenuItem(
+                                value: items.val,
+                                child: Text(items.val!),
+                              );
+                            }).toList(),
+                            // After selecting the desired option,it will
+                            // change button value to selected value
+                            onChanged: (String? newValue) {
+                              setState(() {
+                                dropdownvalue = newValue!;
+                                getDropdown2Value(dropdownvalue!);
+                              });
+                            },
+                          ))));
                 } else {
                   return const CircularProgressIndicator();
                 }
               },
             ),
-
             FutureBuilder<List<OptionModel>?>(
               future: getDropdown2Value(dropdownvalue ?? ""),
               builder: (context, snapshot) {
                 if (snapshot.hasData) {
                   var data = snapshot.data!;
-                  print("data ${data}  ${dropdownData2.length}  ${dropdownvalue2}");
-                  return
-                    Container(
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          border: Border.all(),
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        child: Padding(
-                            padding: const EdgeInsets.all(5.0),
-                            child: DropdownButtonHideUnderline(
-                                child:
-                                DropdownButton(
-                                  // Initial Value
-                                  // value: dropdownvalue2 != "" ? data[0].val : "",
-                                  value:  data[0].val,
+                  print(
+                      "data ${data}  ${dropdownData2.length}  ${dropdownvalue2}");
+                  return Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        border: Border.all(),
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Padding(
+                          padding: const EdgeInsets.all(5.0),
+                          child: DropdownButtonHideUnderline(
+                              child: DropdownButton(
+                            // Initial Value
+                            // value: dropdownvalue2 != "" ? data[0].val : "",
+                            value: data[0].val,
 
-                                  icon: const Icon(Icons.keyboard_arrow_down),
-                                  // Array list of items
-                                  items: data.map<DropdownMenuItem<String>>((OptionModel items) {
-                                    return DropdownMenuItem(
-                                      value: items.val != null ? items.val : null,
-                                      // value: items.val,
-                                      child: Text(items.val!),
-                                    );
-                                  }).toList(),
-                                  // After selecting the desired option,it will
-                                  // change button value to selected value
-                                  onChanged: (String? newValue) {
-                                    setState(() {
-                                      dropdownvalue2 = newValue!;
-                                    });
-                                  },
-                                ))));
+                            icon: const Icon(Icons.keyboard_arrow_down),
+                            // Array list of items
+                            items: data.map<DropdownMenuItem<String>>(
+                                (OptionModel items) {
+                              return DropdownMenuItem(
+                                value: items.val != null ? items.val : null,
+                                // value: items.val,
+                                child: Text(items.val!),
+                              );
+                            }).toList(),
+                            // After selecting the desired option,it will
+                            // change button value to selected value
+                            onChanged: (String? newValue) {
+                              setState(() {
+                                dropdownvalue2 = newValue!;
+                              });
+                            },
+                          ))));
                 } else {
                   return const CircularProgressIndicator();
                 }
@@ -223,6 +210,7 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+
 class OptionModel {
   String? id;
   String? val;
